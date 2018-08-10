a8c6a909012cb1facb62b667504544d833e17475
sudeen <ranjitkarsudeen14@gmail.com>
2018-08-10 23:34:37 +1000

    Updated CustomUserDetails class

--- spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/model/CustomUserDetails.java
+++ spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/model/CustomUserDetails.java
@@ -1,9 +1,11 @@
 package com.sudin.security.springsecurityauthserver.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.stream.Collectors;
 
 public class CustomUserDetails extends Users implements UserDetails {
 
@@ -13,36 +15,39 @@ public class CustomUserDetails extends Users implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return getRoles()
+                .stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" +role.getRole()))
+                .collect(Collectors.toList());
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return super.getPassword();
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return super.getName();
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return true;
     }
 }
