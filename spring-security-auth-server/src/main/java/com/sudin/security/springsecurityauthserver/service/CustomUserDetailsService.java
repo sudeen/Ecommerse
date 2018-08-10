79cfc207f55a24776cdcce41890bf611cebdc526
sudeen <ranjitkarsudeen14@gmail.com>
2018-08-10 23:58:12 +1000

    modified custom user detail service

--- spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/service/CustomUserDetailsService.java
+++ spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/service/CustomUserDetailsService.java
@@ -9,7 +9,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
-import java.net.UnknownServiceException;
+
 import java.util.Optional;
 
 @Service
