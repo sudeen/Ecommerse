0d57148adcf1d66c5ba9bd96ed6025f2f202f890
sudeen <ranjitkarsudeen14@gmail.com>
2018-08-10 23:49:45 +1000

    added mysql properties

--- spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/repository/UserRepository.java
+++ spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.sudin.security.springsecurityauthserver.repository;
 import com.sudin.security.springsecurityauthserver.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<Users,Integer> {
+    Optional<Users> findByName(String username);
 }
--- spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/service/CustomUserDetailsService.java
+++ spring-security-auth-server/src/main/java/com/sudin/security/springsecurityauthserver/service/CustomUserDetailsService.java
@@ -1,14 +1,29 @@
 package com.sudin.security.springsecurityauthserver.service;
 
+import com.sudin.security.springsecurityauthserver.model.CustomUserDetails;
+import com.sudin.security.springsecurityauthserver.model.Users;
+import com.sudin.security.springsecurityauthserver.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.net.UnknownServiceException;
+import java.util.Optional;
+
 @Service
 public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Users> usersOptional = userRepository.findByName(username);
+        usersOptional
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found!"));
+        return usersOptional.map(CustomUserDetails::new)
+                .get();
     }
 }
